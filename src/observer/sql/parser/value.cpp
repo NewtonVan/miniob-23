559a9f4d81622aca8d8b12752747acf11eb27618
huyongqii <yongqi_hu@163.com>
2023-10-26 20:14:21 +0800

    add text unique multil-index

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -242,9 +242,9 @@ void Value::set_float(float val)
 void Value::set_text(const char *s)
 {
     attr_type_ = TEXTS;
-    strncpy(text_value_, s, 65534);
-    text_value_[65534] = '\0'; // 确保字符数组以 null 终止
-    length_ = 65535;
+    strncpy(text_value_, s, 4096);
+    text_value_[4095] = '\0'; // 确保字符数组以 null 终止
+    length_ = 4096;
 }
 
 void Value::set_boolean(bool val)
